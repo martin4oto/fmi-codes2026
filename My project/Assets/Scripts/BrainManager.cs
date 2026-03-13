9036436472d4640d06e44ba8ba98da2180924ea0
Christopher <alhokolik102@gmail.com>
2026-03-13 22:36:26 +0200

    Brain HP

--- My project/Assets/Scripts/BrainManager.cs
+++ My project/Assets/Scripts/BrainManager.cs
@@ -1,11 +1,37 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BrainManager : MonoBehaviour
 {
     public static BrainManager instance;
 
+    public Event gameOver;
+
+    [SerializeField]
+    private float hp;
+
+    [SerializeField]
+    private Slider hpBar;
+
     private void Awake()
     {
         instance = this;
+
+        hpBar.maxValue = hp;
+        hpBar.value = hp;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hp -= damage;
+        hpBar.value = hp;
+
+        if (hp <= 0) Die();
+    }
+
+    private void Die()
+    {
+        //die
     }
 }
