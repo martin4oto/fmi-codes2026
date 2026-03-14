795314c8a83813c9a887ca732200c4c9be040cfc
Christopher <alhokolik102@gmail.com>
2026-03-14 11:19:01 +0200

    Cell Spawning

--- My project/Assets/Scripts/CellSpawner.cs
+++ My project/Assets/Scripts/CellSpawner.cs
@@ -2,19 +2,22 @@ using AYellowpaper.SerializedCollections;
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CellSpawner : MonoBehaviour
 {
     [SerializeField]
-    private List<Transform> upSpawnLocations;
+    private List<Transform> firstCornerSpawnLocations;
     [SerializeField]
-    private List<Transform> downSpawnLocations;
+    private List<Transform> secondCornerSpawnLocations;
     [SerializeField]
-    private List<Transform> leftSpawnLocations;
+    private List<Transform> thirdCornerSpawnLocations;
     [SerializeField]
-    private List<Transform> rightSpawnLocations;
+    private List<Transform> fourthCornerSpawnLocations;
     [SerializedDictionary("Cell Type", "Prefab")]
-    public SerializedDictionary<string, Cell> cells;
+    public AYellowpaper.SerializedCollections.SerializedDictionary<ActiveCell, Cell> cells;
+
+    private ActiveCell activeCellSpawing;
 
     private void Update()
     {
@@ -26,22 +29,47 @@ public class CellSpawner : MonoBehaviour
             Debug.Log(CalculateSpawnDirection());
             InputManager.instance.UseTestInput();
         }
+
+        if (InputManager.instance.SpawnCell1Input)
+        {
+            activeCellSpawing = ActiveCell.basic;
+        }
+        else if (InputManager.instance.SpawnCell2Input)
+        {
+            activeCellSpawing = ActiveCell.ranged;
+        }
+        else if (InputManager.instance.SpawnCell3Input)
+        {
+            activeCellSpawing = ActiveCell.bomb;
+        }
+        else if (InputManager.instance.SpawnCell4Input)
+        {
+            activeCellSpawing = ActiveCell.spawner;
+        }
     }
 
     private void SpawnCell()
     {
-        
+        Vector2 spawnPos = GetSpawnPosition();
+        var cellObj = Instantiate(cells[activeCellSpawing], spawnPos, Quaternion.identity);
+        var cell = cellObj.GetComponent<Cell>();
     }
 
     private Vector2 GetSpawnPosition()
     {
         Vector2 direction = CalculateSpawnDirection();
-        Vector2 spawnPos = CalculateSpawnDirection();
+        Vector2 spawnPos = Vector2.zero;
 
-        if (direction.x < 0) spawnPos = DetermineSpawnLocation(leftSpawnLocations);
-        else if (direction.x > 0) spawnPos = DetermineSpawnLocation(rightSpawnLocations);
-        else if (direction.y < 0) spawnPos = DetermineSpawnLocation(downSpawnLocations);
-        else spawnPos = DetermineSpawnLocation(upSpawnLocations);
+        if (direction.x < 0)
+        {
+            if (direction.y < 0) spawnPos = DetermineSpawnLocation(thirdCornerSpawnLocations);
+            else spawnPos = DetermineSpawnLocation(firstCornerSpawnLocations);
+        }
+        else
+        {
+            if (direction.y < 0) spawnPos = DetermineSpawnLocation(fourthCornerSpawnLocations);
+            else spawnPos = DetermineSpawnLocation(secondCornerSpawnLocations);
+        }
 
         return spawnPos;
     }
@@ -51,16 +79,24 @@ public class CellSpawner : MonoBehaviour
         Vector2 direction = Vector2.zero;
         Vector2 mousePos = InputManager.instance.MouseRelativeToBrainPosition;
 
-        if (Mathf.Abs(mousePos.x) >= Mathf.Abs(mousePos.y * 2)) direction.x = Mathf.Sign(mousePos.x);
-        else direction.y = Mathf.Sign(mousePos.y);
+        direction.x = Mathf.Sign(mousePos.x);
+        direction.y = Mathf.Sign(mousePos.y);
 
         return direction;
     }
 
     private Vector2 DetermineSpawnLocation(List<Transform> positions)
     {
-        int chosenPosition = UnityEngine.Random.RandomRange(0, positions.Count);
+        int chosenPosition = Random.Range(0, positions.Count);
 
         return positions[chosenPosition].position;
     }
+}
+
+public enum ActiveCell
+{
+    basic,
+    bomb,
+    ranged,
+    spawner
 }
