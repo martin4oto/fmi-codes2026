2fd6f8a5fcdff7c462291d1fdc0d6df3b32a7498
Christopher <alhokolik102@gmail.com>
2026-03-15 15:29:38 +0200

    HPBarBoss

--- My project/Assets/Scripts/Cells/bosses/spiderBoss.cs
+++ My project/Assets/Scripts/Cells/bosses/spiderBoss.cs
@@ -146,4 +146,10 @@ public class spiderBoss:Cell
         CellManager.instance.AddVirus(projectileCell);
     }
 
+    public override void TakeDamage(int DMG)
+    {
+        base.TakeDamage(DMG);
+
+        hpBar.value = HP;
+    }
 }
